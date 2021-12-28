12bdba6eb635d7e299a6fe59c6c500f7defda4e9
David  Paula <davidpaulati@gmail.com>
2021-12-28 15:27:32 -0300

    Estudo de JS no discover da Rocketseat

---
+++ if_else.js
@@ -0,0 +1,11 @@
+//if...else
+
+let temperature = 38
+
+if(temperature >= 37.5){
+    console.log('Febre Alta')
+} else if (temperature < 37.5 && temperature >= 37) {
+    console.log('Febre moderada')
+} else {
+    console.log('Normal')
+}
---
+++ switch.js
@@ -0,0 +1,14 @@
+//switch
+let expression = 'a'
+switch(expression) {
+    case 'a':
+        console.log('a')
+        break
+    case 'b':
+        console.log('b')
+        break
+    default:
+        console.log('default')
+        break
+
+}
